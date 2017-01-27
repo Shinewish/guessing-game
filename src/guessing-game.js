2cd20ea98506b5d7029d90d1aa0d7fad7a2698b4
AlexTheSunbeam <aliaksandr.tachko@gmail.com>
2017-01-27 23:20:08 +0300

    Tests worked

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.lastGuess = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	var promp = Math.floor((this.min + this.max + 1) / 2);
+    	this.lastGuess = promp;
+    	
+    	return promp;
     }
 
     lower() {
-
+    	this.max = this.lastGuess;
     }
 
     greater() {
-
+    	this.min = this.lastGuess;
     }
 }
 
